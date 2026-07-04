9b0bcfb363f1b8891e7e83dfe34025e4c711ce34
al3xmm14 <molinaa757@gmail.com>
2026-07-03 20:16:08 -0600

    feat: habilitar y mejorar desenfoque (blur) en la barra superior
    - Configurar blur-method, blur-size y blur-strength en picom.conf.
    - Habilitar blur-background para ventanas tipo 'dock' en las reglas de picom.
    - Ajustar opacidad y color de fondo de la barra en barStyle.py para visualizar el blur.

--- styles/barStyle.py
+++ styles/barStyle.py
@@ -1,9 +1,9 @@
 def get_bar_style():
     bar_style = {
-    "background": "#00000000",
+    "background": "#00000080",
     "margin": [10, 7, 10, 7],
     "border_color": ["ffffff", "ffffff", "ffffff", "ffffff"],
-    "opacity": 1,
+    "opacity": 0.8,
     }    
     return bar_style
 
